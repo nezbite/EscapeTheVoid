f5488dfd26d3358317e990e230c5a1c04171ec6c
Daniel <nesbite85@gmail.com>
2024-08-04 19:39:14 +0200

    added side map elements + back view

--- project/src/main/kotlin/cga/exercise/components/map/MapManager.kt
+++ project/src/main/kotlin/cga/exercise/components/map/MapManager.kt
@@ -10,22 +10,37 @@ import kotlin.random.Random
 class MapManager {
     val MAP_SIZE = 40
     val SEGMENT_SIZE = 3*6
+    val FAR_SEGMENT_SIZE = 3*6*2
+
     var segments: Array<MapSegment> = Array(MAP_SIZE) { MapSegment(Renderable(mutableListOf())) }
+    var farSegments: Array<MapSegment> = Array(MAP_SIZE) { MapSegment(Renderable(mutableListOf())) }
+
     var segmentIds = mutableListOf<Int>()
+    var farSegmentIds = mutableListOf<Int>()
+
     lateinit var random: Random
 
     var currentSegment = 0 // Current segment the player is on
     var mapSegment = 0 // Current starting segment of the map
 
     var roadModels = mutableListOf<Renderable>()
+    var farModels = mutableListOf<Renderable>()
 
     fun init(seed: Int = 0) {
         random = Random(seed)
         segments = Array(MAP_SIZE) { MapSegment(Renderable(mutableListOf())) }
+        farSegments = Array(MAP_SIZE) { MapSegment(Renderable(mutableListOf())) }
+
         segmentIds = mutableListOf()
         segmentIds.add(3)
         for (i in 1 .. segments.size) {
             segmentIds.add(getNextMapSegment(segmentIds.last()))
+        }
+
+        farSegmentIds = mutableListOf()
+        farSegmentIds.add(0)
+        for (i in 1 .. farSegments.size) {
+            farSegmentIds.add(random.nextInt(farModels.size))
         }
         setSegments()
     }
@@ -37,6 +52,11 @@ class MapManager {
             if (segmentIds.size < currentSegment + MAP_SIZE) {
                 for (i in segmentIds.size .. currentSegment + MAP_SIZE-1) {
                     segmentIds.add(getNextMapSegment(segmentIds.last()))
+                }
+            }
+            if (farSegmentIds.size < currentSegment/2 + MAP_SIZE) {
+                for (i in farSegmentIds.size .. currentSegment/2 + MAP_SIZE-1) {
+                    farSegmentIds.add(random.nextInt(farModels.size))
                 }
             }
             setSegments()
@@ -53,6 +73,16 @@ class MapManager {
             segment.position = currentSegment+i-2
             segment.renderable.setPosition(Vector3f(0f, 0f, segment.position*SEGMENT_SIZE.toFloat()))
             segments[i] = segment
+        }
+        farSegments = Array(farSegments.size) { MapSegment(Renderable(mutableListOf())) }
+        for (i in 0 .. farSegments.size-1) {
+            if (currentSegment < 0) {
+                currentSegment = 0
+            }
+            val segment = MapSegment(farModels[farSegmentIds[currentSegment/2+i]].copy())
+            segment.position = currentSegment/2+i-2
+            segment.renderable.setPosition(Vector3f(0f, 0f, segment.position*FAR_SEGMENT_SIZE.toFloat()))
+            farSegments[i] = segment
         }
     }
 
